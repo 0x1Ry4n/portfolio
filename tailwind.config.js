74035b5b8b25d02431d3c9e3cd17f697e9c97580
Ryan <ryanlucas2018gz@gmail.com>
2023-02-07 02:39:58 -0300

    Configs

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+      boxShadow: {
+        "3xl": "0px 5px 10px 0px rgb(178, 130, 40, 0.6)",
+        "3xlh": "0px 5px 12px 2px rgb(178, 130, 40, 0.8)",
+      },
+      colors: {
+        gold: "#B28228",
+        mediumGray: "text-gray-600",
+        transparentGray: "rgb(40, 39, 42, 0.9)",
+      },
+      keyframes: {
+        float: {
+          "0%: 100": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(-20px)" },
+        },
+      },
+      animation: {
+        floatAnimation: "float 4s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
